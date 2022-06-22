3efd60bdcfa71f82fb9c552eb5b5ce9a450d7a7c
William Woodruff <william@yossarian.net>
2022-06-21 22:09:39 -0400

    stubs: add SSH stubs, update others, format

--- pyrage-stubs/pyrage-stubs/pyrage.pyi
+++ pyrage-stubs/pyrage-stubs/pyrage.pyi
@@ -1,12 +1,9 @@
 from typing import Sequence, Union
 
-from . import x25519
+from . import ssh, x25519
 
-Identity = Union[x25519.Identity]
-Recipient = Union[x25519.Recipient]
-
+Identity = Union[ssh.Identity, x25519.Identity]
+Recipient = Union[ssh.Recipient, x25519.Recipient]
 
 def encrypt(plaintext: bytes, identities: Sequence[Identity]) -> bytes: ...
-
-
 def decrypt(ciphertext: bytes, recipients: Sequence[Recipient]) -> bytes: ...
