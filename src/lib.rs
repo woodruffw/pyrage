79b4e51d03d74bacc8a46e977b7d013c44808714
William Woodruff <william@yossarian.net>
2022-08-03 18:28:49 -0400

    lib: custom encrypt/decrypt exceptions
    Signed-off-by: William Woodruff <william@yossarian.net>

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,14 @@
 
 use std::io::{Read, Write};
 
-use age::{DecryptError, EncryptError, Encryptor, Identity, Recipient};
+use age::{
+    DecryptError as RageDecryptError, EncryptError as RageEncryptError, Encryptor, Identity,
+    Recipient,
+};
 use age_core::format::{FileKey, Stanza};
 use pyo3::{
-    exceptions::{PyTypeError, PyValueError},
+    create_exception,
+    exceptions::{PyException, PyTypeError},
     prelude::*,
     py_run,
     types::PyBytes,
@@ -39,7 +43,7 @@ macro_rules! recipient_traits {
     ($($t:ty),+) => {
         $(
             impl Recipient for $t {
-                fn wrap_file_key(&self, file_key: &FileKey) -> Result<Vec<Stanza>, EncryptError> {
+                fn wrap_file_key(&self, file_key: &FileKey) -> Result<Vec<Stanza>, RageEncryptError> {
                     self.0.wrap_file_key(file_key)
                 }
             }
@@ -63,7 +67,7 @@ macro_rules! identity_traits {
     ($($t:ty),+) => {
         $(
             impl Identity for $t {
-                fn unwrap_stanza(&self, stanza: &Stanza) -> Option<Result<FileKey, DecryptError>> {
+                fn unwrap_stanza(&self, stanza: &Stanza) -> Option<Result<FileKey, RageDecryptError>> {
                     self.0.unwrap_stanza(stanza)
                 }
             }
@@ -116,6 +120,8 @@ impl<'source> FromPyObject<'source> for Box<dyn PyrageIdentity> {
     }
 }
 
+create_exception!(pyrage, EncryptError, PyException);
+
 #[pyfunction]
 fn encrypt<'p>(
     py: Python<'p>,
@@ -131,17 +137,19 @@ fn encrypt<'p>(
     let mut encrypted = vec![];
     let mut writer = encryptor
         .wrap_output(&mut encrypted)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| EncryptError::new_err(e.to_string()))?;
     writer
         .write_all(plaintext)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| EncryptError::new_err(e.to_string()))?;
     writer
         .finish()
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| EncryptError::new_err(e.to_string()))?;
 
     // TODO: Avoid this copy. Maybe PyBytes::new_with?
     Ok(PyBytes::new(py, &encrypted))
 }
+
+create_exception!(pyrage, DecryptError, PyException);
 
 #[pyfunction]
 fn decrypt<'p>(
@@ -152,10 +160,10 @@ fn decrypt<'p>(
     let identities = identities.iter().map(|pi| pi.as_ref().as_identity());
 
     let decryptor =
-        match age::Decryptor::new(ciphertext).map_err(|e| PyValueError::new_err(e.to_string()))? {
+        match age::Decryptor::new(ciphertext).map_err(|e| DecryptError::new_err(e.to_string()))? {
             age::Decryptor::Recipients(d) => d,
             age::Decryptor::Passphrase(_) => {
-                return Err(PyValueError::new_err(
+                return Err(DecryptError::new_err(
                     "invalid ciphertext (encrypted with passphrase, not identities)",
                 ))
             }
@@ -164,10 +172,10 @@ fn decrypt<'p>(
     let mut decrypted = vec![];
     let mut reader = decryptor
         .decrypt(identities)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| DecryptError::new_err(e.to_string()))?;
     reader
         .read_to_end(&mut decrypted)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| DecryptError::new_err(e.to_string()))?;
 
     // TODO: Avoid this copy. Maybe PyBytes::new_with?
     Ok(PyBytes::new(py, &decrypted))
@@ -198,7 +206,9 @@ fn pyrage(py: Python, m: &PyModule) -> PyResult<()> {
     );
     m.add_submodule(passphrase)?;
 
+    m.add("EncryptError", py.get_type::<EncryptError>())?;
     m.add_wrapped(wrap_pyfunction!(encrypt))?;
+    m.add("DecryptError", py.get_type::<DecryptError>())?;
     m.add_wrapped(wrap_pyfunction!(decrypt))?;
 
     Ok(())
