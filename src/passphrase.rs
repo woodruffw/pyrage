a9413d4e749147f1d079e863832e1569ca9a949c
William Woodruff <william@yossarian.net>
2022-08-03 18:35:17 -0400

    passphrase: use custom exceptions
    Signed-off-by: William Woodruff <william@yossarian.net>

--- src/passphrase.rs
+++ src/passphrase.rs
@@ -1,7 +1,9 @@
 use std::io::{Read, Write};
 
 use age::{secrecy::Secret, Decryptor, Encryptor};
-use pyo3::{exceptions::PyValueError, prelude::*, types::PyBytes};
+use pyo3::{prelude::*, types::PyBytes};
+
+use crate::{DecryptError, EncryptError};
 
 #[pyfunction]
 fn encrypt<'p>(py: Python<'p>, plaintext: &[u8], passphrase: &str) -> PyResult<&'p PyBytes> {
@@ -9,13 +11,13 @@ fn encrypt<'p>(py: Python<'p>, plaintext: &[u8], passphrase: &str) -> PyResult<&
     let mut encrypted = vec![];
     let mut writer = encryptor
         .wrap_output(&mut encrypted)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| EncryptError::new_err(e.to_string()))?;
     writer
         .write_all(plaintext)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| EncryptError::new_err(e.to_string()))?;
     writer
         .finish()
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| EncryptError::new_err(e.to_string()))?;
 
     Ok(PyBytes::new(py, &encrypted))
 }
@@ -23,10 +25,10 @@ fn encrypt<'p>(py: Python<'p>, plaintext: &[u8], passphrase: &str) -> PyResult<&
 #[pyfunction]
 fn decrypt<'p>(py: Python<'p>, ciphertext: &[u8], passphrase: &str) -> PyResult<&'p PyBytes> {
     let decryptor =
-        match Decryptor::new(ciphertext).map_err(|e| PyValueError::new_err(e.to_string()))? {
+        match Decryptor::new(ciphertext).map_err(|e| DecryptError::new_err(e.to_string()))? {
             Decryptor::Passphrase(d) => d,
             _ => {
-                return Err(PyValueError::new_err(
+                return Err(DecryptError::new_err(
                     "invalid ciphertext (not passphrase encrypted)",
                 ))
             }
@@ -34,10 +36,10 @@ fn decrypt<'p>(py: Python<'p>, ciphertext: &[u8], passphrase: &str) -> PyResult<
     let mut decrypted = vec![];
     let mut reader = decryptor
         .decrypt(&Secret::new(passphrase.into()), None)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| DecryptError::new_err(e.to_string()))?;
     reader
         .read_to_end(&mut decrypted)
-        .map_err(|e| PyValueError::new_err(e.to_string()))?;
+        .map_err(|e| DecryptError::new_err(e.to_string()))?;
 
     Ok(PyBytes::new(py, &decrypted))
 }
